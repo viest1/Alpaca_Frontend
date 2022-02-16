be602c2194708e5aadacaaf87588896c7fd7375a
viest1 <viest1994@gmail.com>
2022-02-16 10:06:26 +0100

    first commit

---
+++ src/view/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from '../components/templates/HomePage/HomePage';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
