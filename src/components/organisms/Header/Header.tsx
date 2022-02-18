7185bf3d8839eafd22734ccb2885fb3e59a2e3b1
viest1 <viest1994@gmail.com>
2022-02-18 18:04:30 +0100

    working on Header, created NavLink Component and added data for NavLinks

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,84 +1,119 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { GrClose } from 'react-icons/gr';
 import CompanyLogo from '../../../assets/illustrations/COMPANYLOGO.png';
+import NavLink from '../../atoms/NavLink/NavLink';
 
-export const Alpaca = styled.div`
+interface StyledDivProps {
+  isOpenMenu: boolean;
+}
+
+const Container = styled.div<StyledDivProps>`
   background: ${({ theme }) => theme.color.main1};
   color: ${({ theme }) => theme.color.main2};
-  display: flex;
-  justify-content: flex-end;
-  &:first-child {
-    justify-content: flex-start;
-  }
-  padding: 2rem;
   font-size: ${({ theme }) => theme.fontSizeInter.xl};
   border: 2px solid black;
+  min-height: ${({ isOpenMenu }) => (isOpenMenu ? '100vh' : 'auto')};
 `;
 
-export const Logo = styled.img`
-  padding: 2rem;
-  font-size: 3rem;
-`;
-export const AboutUs = styled.div`
-  padding: 2rem;
+const Flex = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 0.3rem 1rem 0 1rem;
+  align-items: center;
 `;
-export const Plans = styled.div`
-  padding: 2rem;
+
+const FlexOpen = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid grey;
+  padding: 0.3rem 1rem;
 `;
-export const Login = styled.div`
-  padding: 2rem;
+
+const StyledP = styled.p`
+  font-size: ${({ theme }) => theme.fontSizeOpenSans.xxxs};
+  position: absolute;
+  bottom: 10px;
+  right: -43px;
+  font-weight: bold;
 `;
-export const SingUp = styled.div`
-  padding: 2rem;
-`;
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: ${({ theme }) => theme.color.main2};
-`;
+
+const data = [
+  {
+    path: 'aboutUs',
+    text: 'About us',
+    id: 1
+  },
+  {
+    path: 'plans',
+    text: 'Plans',
+    id: 2
+  },
+  {
+    path: 'login',
+    text: 'Login',
+    id: 3
+  },
+  {
+    path: 'signup',
+    text: 'Sign up',
+    id: 4
+  },
+  {
+    path: 'aboutUs',
+    text: 'About us',
+    id: 5
+  },
+  {
+    path: '/',
+    text: '',
+    image: CompanyLogo,
+    alt: 'Logo',
+    id: 6
+  }
+];
 
 function Header() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
-  const toogle = () => {
+  const handleOpenMenu = () => {
     setIsOpenMenu((prev) => !prev);
   };
-  // const handleOpenMenu = () => {
-  //   setIsOpenMenu(true);
-  // };
-  // const handleCloseMenu = () => {
-  //   setIsOpenMenu(false);
-  // };
-  return (
-    <Alpaca>
-      <StyledLink to="/">
-        <Logo src={CompanyLogo} />
-      </StyledLink>
 
-      {isOpenMenu ? null : <GiHamburgerMenu onClick={toogle} />}
-      {isOpenMenu ? (
+  return (
+    <Container isOpenMenu={isOpenMenu}>
+      {!isOpenMenu && (
+        <Flex>
+          <div style={{ position: 'relative' }}>
+            <NavLink path="/" image={CompanyLogo} alt="Logo" />
+            <StyledP>live outside the box</StyledP>
+          </div>
+          <div>{!isOpenMenu && <GiHamburgerMenu fontSize={48} onClick={handleOpenMenu} />}</div>
+        </Flex>
+      )}
+      {isOpenMenu && (
         <>
-          <GrClose onClick={toogle} />
-          <StyledLink to="/about-us">
-            <AboutUs>About Us</AboutUs>
-          </StyledLink>
-          <StyledLink to="/plans">
-            <Plans>Plans</Plans>
-            <StyledLink to="/login">
-              <Login>Login</Login>
-            </StyledLink>
-            <StyledLink to="/sing-up">
-              <SingUp>Sign up</SingUp>
-            </StyledLink>
-            <StyledLink to="/">
-              <Logo src={CompanyLogo} />
-            </StyledLink>
-          </StyledLink>
+          <FlexOpen>
+            <div style={{ position: 'relative' }}>
+              <NavLink path="/" image={CompanyLogo} alt="Logo" />
+              <StyledP>live outside the box</StyledP>
+            </div>
+            <div>
+              <GrClose onClick={handleOpenMenu} />
+            </div>
+          </FlexOpen>
+          {data.map((item) => (
+            <NavLink
+              key={item.id}
+              path={item.path}
+              text={item.text}
+              image={item.image}
+              alt={item.alt}
+            />
+          ))}
         </>
-      ) : null}
-    </Alpaca>
+      )}
+    </Container>
   );
 }
 
