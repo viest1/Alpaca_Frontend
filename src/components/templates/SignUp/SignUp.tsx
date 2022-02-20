0fd6e519ffa3c90179c15640d2c5f39a1f8185bb
shanny <foncham.andin@yahoo.com>
2022-02-20 11:20:39 +0100

    add stub for signup

---
+++ src/components/templates/SignUp/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../organisms/Header/Header';
+import './SignUp.css';
+
+function SignUp() {
+  return (
+    <div id="sign-up">
+      <Header />
+      signup
+    </div>
+  );
+}
+
+export default SignUp;
