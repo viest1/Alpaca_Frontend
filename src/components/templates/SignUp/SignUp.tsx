911eec9330b675eb7bc902957569824765ab56d0
shanny <foncham.andin@yahoo.com>
2022-02-21 12:09:29 +0100

    working on signup form

--- src/components/templates/SignUp/SignUp.tsx
+++ src/components/templates/SignUp/SignUp.tsx
@@ -3,10 +3,57 @@ import Header from '../../organisms/Header/Header';
 import './SignUp.css';
 
 function SignUp() {
+  //   <form
+  //   onSubmit={(e: React.SyntheticEvent) => {
+  //     e.preventDefault();
+  //     const target = e.target as typeof e.target & {
+  //       name: { value: string };
+  //       surname: { value: string };
+  //       email: { value: string };
+  //       password: { value: string };
+  //       repeatPassword: { value: string };
+  //       phoneNumber: { value: number };
+  //     };
+  //     const email = target.email.value;
+  //     const password = target.password.value;
+  //   }}
+  // >
+
   return (
     <div id="sign-up">
       <Header />
-      signup
+      <div>
+        <h1>CREATE NEW ACCOUNT</h1>
+        <img src="" alt="" />
+      </div>
+      <h2>Contact information</h2>
+      <div>
+        <label htmlFor="name">
+          Name:
+          <input type="name" name="name" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="surname">
+          Surname:
+          <input type="surname" name="surname" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" />
+        </label>
+      </div>
+      <div>
+        <input type="submit" value="Log in" />
+      </div>
     </div>
   );
 }
