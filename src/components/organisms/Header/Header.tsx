a522990b85e1e1808147bd8d0bb82647c2c2b241
Gabo984 <fernandezmendozagabriel1@gmail.com>
2022-02-28 13:06:07 +0100

    Header client added and route of logout

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -135,29 +135,63 @@ const StyledInput = styled.input`
   margin: auto;
   border-radius: 0.3rem;
 `;
+// Style ClientHeader
+const ContainerDesktopClient = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: ${({ theme }) => theme.color.main7};
+  color: ${({ theme }) => theme.color.main8};
+  font-size: ${({ theme }) => theme.fontSizeOpenSans.m};
+  padding: 0.2rem;
+`;
+
+const StyledMenuDesktopClient = styled.div`
+  display: flex;
+  margin: auto;
+  flex-direction: row;
+  gap: 2rem;
+  font-weight: bold;
+`;
+const CountryFlagClient = styled.div`
+  display: flex;
+  gap: 0.1rem;
+  margin: auto;
+  :hover {
+    cursor: pointer;
+  }
+`;
+const ServicesAndLanguageClient = styled.div`
+  display: flex;
+  position: relative;
+  padding-right: 32px;
+  gap: 1rem;
+  justify-content: space-around;
+  margin: 1rem;
+`;
+
 const data = [
   {
-    path: 'aboutUs',
+    path: '/aboutUs',
     text: 'ABOUT US',
     id: 1
   },
   {
-    path: 'plans',
-    text: 'PLANS',
+    path: '/services',
+    text: 'SERVICES',
     id: 2
   },
   {
-    path: 'login',
+    path: '/login',
     text: 'LOGIN',
     id: 3
   },
   {
-    path: 'signUp',
+    path: '/signup',
     text: 'SIGN UP',
     id: 4
   },
   {
-    path: 'contact',
+    path: '/contact',
     text: 'CONTACT',
     id: 5
   }
@@ -165,22 +199,22 @@ const data = [
 
 const dataDesktop = [
   {
-    path: 'aboutUs',
+    path: '/aboutUs',
     text: 'ABOUT US',
     id: 1
   },
   {
-    path: 'services',
+    path: '/services',
     text: 'SERVICES',
     id: 2
   },
   {
-    path: 'contact',
+    path: '/contact',
     text: 'CONTACT',
     id: 3
   },
   {
-    path: 'login',
+    path: '/login',
     text: 'LOGIN',
     id: 4
   }
@@ -192,17 +226,34 @@ const dataHeaderAdmin = [
     id: 1
   },
   {
-    path: 'clients',
+    path: '/clients',
     text: 'CLIENTS/PROJECTS',
     id: 2
   },
   {
-    path: 'settings',
+    path: '/settings',
     text: 'SETTINGS',
-    id: 4
+    id: 3
   }
 ];
 
+const dataHeaderClient = [
+  {
+    path: '/',
+    text: 'DASHBOARD',
+    id: 1
+  },
+  {
+    path: '/projects',
+    text: 'PROJECTS',
+    id: 2
+  },
+  {
+    path: '/settings',
+    text: 'SETTINGS',
+    id: 3
+  }
+];
 interface HeaderI {
   displayTimeToLogout: boolean;
 }
@@ -236,6 +287,79 @@ function Header({ displayTimeToLogout }: HeaderI) {
 
   // Testing AdminHeader
   // const adminLogIn = true;
+  if (userData.token && userData.role === 'Client') {
+    return (
+      <div>
+        {!desktopVersion ? (
+          <Container isOpenMenu={isOpenMenu}>
+            {!isOpenMenu && (
+              <Flex>
+                <div style={{ position: 'relative' }}>
+                  <NavLink path="/" image={CompanyLogo} alt="Logo" />
+                  <StyledP>live outside the box</StyledP>
+                </div>
+                <div>
+                  {!isOpenMenu && (
+                    <GiHamburgerMenu fontSize={48} cursor="pointer" onClick={handleOpenMenu} />
+                  )}
+                </div>
+              </Flex>
+            )}
+            {isOpenMenu && (
+              <>
+                <FlexOpen>
+                  <div style={{ position: 'relative' }}>
+                    <NavLink path="/" image={CompanyLogo} alt="Logo" />
+                    <StyledP>live outside the box</StyledP>
+                  </div>
+                  <div>
+                    <GrClose onClick={handleOpenMenu} cursor="pointer" fontSize={48} />
+                  </div>
+                </FlexOpen>
+                <StyledMenu>
+                  {dataHeaderClient.map((item) => (
+                    <NavLink
+                      key={item.id}
+                      path={item.path}
+                      text={item.text}
+                      onClick={handleOpenMenu}
+                    />
+                  ))}
+                  <NavLink path="/messages" text="MESSAGES" />
+                  <NavLink path="/logout" text="LOGOUT" />
+                </StyledMenu>
+                <br />
+                <StyledLogoSlogan>
+                  <NavLink path="/" bigLogo image={CompanyLogo} alt="Logo" />
+                  <StyledSlogan>live outside the box</StyledSlogan>
+                </StyledLogoSlogan>
+                <Contact />
+              </>
+            )}
+          </Container>
+        ) : (
+          <ContainerDesktopClient>
+            <StyledLogoSlogan>
+              <NavLink path="/" bigLogo image={CompanyLogo} alt="Logo" />
+            </StyledLogoSlogan>
+            <StyledInput type="text" placeholder="" value="" />
+            <ServicesAndLanguageClient>
+              <StyledMenuDesktopClient>
+                {dataHeaderClient.map((item) => (
+                  <NavLink key={item.id} path={item.path} text={item.text} color="white" />
+                ))}
+                <NavLink path="/logout" text="LOGOUT" color="white" />
+              </StyledMenuDesktopClient>
+              <CountryFlagClient>
+                <span className="fi fi-de" />
+                <span>DE</span>
+              </CountryFlagClient>
+            </ServicesAndLanguageClient>
+          </ContainerDesktopClient>
+        )}
+      </div>
+    );
+  }
   if (userData.token && userData.role === 'Freelancer') {
     return (
       <div>
@@ -274,9 +398,9 @@ function Header({ displayTimeToLogout }: HeaderI) {
                       onClick={handleOpenMenu}
                     />
                   ))}
-                  <NavLink path="/" text="NEW CUSTOMER" />
-                  <NavLink path="/" text="MESSAGES" />
-                  <NavLink path="/" text="STATISTICS" />
+                  <NavLink path="/newClient" text="NEW CUSTOMER" />
+                  <NavLink path="/messages" text="MESSAGES" />
+                  <NavLink path="/statistics" text="STATISTICS" />
                   <NavLink path="/" text="LOGOUT" />
                 </StyledMenu>
                 <br />
@@ -370,7 +494,7 @@ function Header({ displayTimeToLogout }: HeaderI) {
                 <NavLink key={item.id} path={item.path} text={item.text} />
               ))}
 
-              <NavLink path="/" text="SIGN UP" border="2px solid black" />
+              <NavLink path="/signup" text="SIGN UP" border="2px solid black" />
             </StyledMenuDesktop>
             <CountryFlag>
               <span className="fi fi-de" />
