202164a606d6bba0385a1d462d1ec8e3b1affd04
Gabo984 <fernandezmendozagabriel1@gmail.com>
2022-03-07 09:08:33 +0100

    versionMobil Project Datails alost finished

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -195,7 +195,7 @@ const ButtonLogoutMobil = styled.button`
   color: ${({ theme }) => theme.color.main2};
   background: none;
   font-size: ${({ theme }) => theme.fontSizeInter.m};
-  top: 21rem;
+  top: 25rem;
   border: none;
   &:hover {
     cursor: pointer;
@@ -309,9 +309,14 @@ const dataHeaderClient = [
     id: 2
   },
   {
+    path: '/messages',
+    text: 'MESSAGES',
+    id: 3
+  },
+  {
     path: '/settings',
     text: 'SETTINGS',
-    id: 3
+    id: 4
   }
 ];
 const dataAvatarMenu = [
