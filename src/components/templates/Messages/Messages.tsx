e4a0af178a5720b107acd5be821c16682c0ad261
viest1 <viest1994@gmail.com>
2022-03-24 10:06:09 +0100

    fix input messages

--- src/components/templates/Messages/Messages.tsx
+++ src/components/templates/Messages/Messages.tsx
@@ -379,7 +379,7 @@ function Messages() {
           <Form onSubmit={handleSubmitMessage}>
             <div>
               <Input
-                margin="50px 0 0.1rem 0"
+                margin="0 0 0.1rem 0"
                 placeholder="Write a Message..."
                 name="message"
                 onChange={handleChange}
